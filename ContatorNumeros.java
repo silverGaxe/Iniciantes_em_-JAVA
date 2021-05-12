1a0a8c4113876ea3583e0ed37e88ad0420b1151c
silverGaxe <83870289+silverGaxe@users.noreply.github.com>
2021-05-12 20:49:07 -0300

    Exemplo de incremento
    um contador que exibe os números de 1 a 10.

---
+++ ContatorNumeros.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class ContatorNumeros {
+    public static void main(String[] args) {
+        int i;
+        for (i=0; i<11; i++){JOptionPane.showMessageDialog(null, i);}
+        {JOptionPane.showMessageDialog(null, "Fim");}
+    }
+}
